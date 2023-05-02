14252dfa8470cc59e3876c4eb45b945f4d498306
Madiha belouadah <117091050+madiha-bel@users.noreply.github.com>
2023-05-02 09:04:15 +0200

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let img =document.querySelector('.image');
+let container= document.querySelector('.container')
+function phones(phone) {
+    img.src =phone
+}
+function colors(e){
+
+    container.style.background=e;
+
+
+}
